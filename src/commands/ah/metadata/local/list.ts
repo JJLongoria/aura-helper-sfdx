23910265ff15b38dcf9506fe43f05766300fe496
Juan José Longoria <juan.longoria.lopez@accenture.com>
2021-12-13 00:10:46 +0100

    add local metadata list command

--- src/commands/ah/metadata/local/list.ts
+++ src/commands/ah/metadata/local/list.ts
@@ -2,12 +2,11 @@ import * as os from 'os';
 import { FlagsConfig, SfdxCommand, flags } from '@salesforce/command';
 import { Messages, SfdxError } from '@salesforce/core';
 import { SFConnector } from '@aurahelper/connector';
-import { CoreUtils, FileChecker, FileWriter, MetadataType, PathUtils } from '@aurahelper/core';
+import { CoreUtils, FileChecker, MetadataDetail, PathUtils } from '@aurahelper/core';
 import { MetadataFactory } from '@aurahelper/metadata-factory';
 import CommandUtils from '../../../../libs/utils/commandUtils';
 const Validator = CoreUtils.Validator;
 const ProjectUtils = CoreUtils.ProjectUtils;
-const Utils = CoreUtils.Utils;
 
 Messages.importMessagesDirectory(__dirname);
 const messages = Messages.loadMessages('aura-helper-sfdx', 'localDescribe');
@@ -25,18 +24,6 @@ export default class Describe extends SfdxCommand {
       required: false,
       helpValue: '<path/to/project/root>',
     }),
-    all: flags.boolean({
-      char: 'a',
-      description: messages.getMessage('allFlagDescription'),
-      exclusive: ['type'],
-    }),
-    type: flags.array({
-      char: 't',
-      delimiter: ',',
-      description: messages.getMessage('typeFlagDescription'),
-      exclusive: ['all'],
-      helpValue: '<MetadataTypeName>[,<MetadataTypeName>...]',
-    }),
     outputfile: flags.filepath({
       description: generalMessages.getMessage('outputPathFlagDescription'),
       helpValue: '<path/to/output/file>',
@@ -52,7 +39,7 @@ export default class Describe extends SfdxCommand {
     apiversion: flags.builtin(),
   };
 
-  public async run(): Promise<{ [key: string]: MetadataType }> {
+  public async run(): Promise<MetadataDetail[]> {
     try {
       this.flags.root = Validator.validateFolderPath(this.flags.root);
     } catch (error) {
@@ -61,9 +48,6 @@ export default class Describe extends SfdxCommand {
     }
     if (!FileChecker.isSFDXRootPath(this.flags.root)) {
       throw new SfdxError(generalMessages.getMessage('projectNotFoundError'));
-    }
-    if (this.flags.all === undefined && this.flags.type === undefined) {
-      throw new SfdxError(messages.getMessage('missingTypesToDescribeError'));
     }
     if (this.flags.outputfile) {
       try {
@@ -81,53 +65,39 @@ export default class Describe extends SfdxCommand {
     } else {
       this.ux.startSpinner(generalMessages.getMessage('gettingAvailableMetadataTypesMessage'));
     }
+    const metadata: MetadataDetail[] = [];
     const metadataDetails = await connector.listMetadataTypes();
-    if (this.flags.progress) {
-      this.ux.log(messages.getMessage('describeLocalTypesMessage'));
-    } else {
-      this.ux.startSpinner(messages.getMessage('describeLocalTypesMessage'));
-    }
     const folderMetadataMap = MetadataFactory.createFolderMetadataMap(metadataDetails);
     const metadataFromFileSystem = MetadataFactory.createMetadataTypesFromFileSystem(
       folderMetadataMap,
-      this.flags.root,
-      this.flags.group
+      this.flags.root
     );
-    let metadata: { [key: string]: MetadataType } = {};
-    if (this.flags.all) {
-      metadata = metadataFromFileSystem;
-    } else if (this.flags.type) {
-      const types = CommandUtils.getTypes(this.flags.type);
-      for (const type of types) {
-        if (metadataFromFileSystem[type]) {
-          metadata[type] = metadataFromFileSystem[type];
-        }
+    Object.keys(folderMetadataMap).forEach(function (folder) {
+      const metadataType = folderMetadataMap[folder];
+      if (metadataFromFileSystem[metadataType.xmlName]) {
+        metadata.push(metadataType);
       }
-    }
+    });
     if (!this.flags.json) {
-      if (metadata && Utils.hasKeys(metadata)) {
+      if (metadataDetails && metadataDetails.length) {
         if (this.flags.csv) {
-          const csvData = CommandUtils.transformMetadataTypesToCSV(metadata);
+          const csvData = CommandUtils.transformMetadataDetailsToCSV(metadata);
           this.ux.log(csvData);
         } else {
-          const datatable = CommandUtils.transformMetadataTypesToTable(metadata);
+          const datatable = CommandUtils.transformMetadataDetailsToTable(metadata);
           this.ux.table(datatable, {
             columns: [
               {
-                key: 'type',
-                label: 'Metadata Type',
+                key: 'name',
+                label: 'Name',
               },
               {
-                key: 'object',
-                label: 'Metadata Object',
+                key: 'directory',
+                label: 'Directory',
               },
               {
-                key: 'item',
-                label: 'Metadata Item',
-              },
-              {
-                key: 'path',
-                label: 'Path',
+                key: 'suffix',
+                label: 'Suffix',
               },
             ],
           });
@@ -135,14 +105,6 @@ export default class Describe extends SfdxCommand {
       } else {
         this.ux.log(messages.getMessage('noDataToShowError'));
       }
-    }
-    if (this.flags.outputfile) {
-      const baseDir = PathUtils.getDirname(this.flags.outputfile);
-      if (!FileChecker.isExists(baseDir)) {
-        FileWriter.createFolderSync(baseDir);
-      }
-      FileWriter.createFileSync(this.flags.outputile, JSON.stringify(metadata, null, 2));
-      this.ux.log(messages.getMessage('outputSavedMessage', [this.flags.outputfile]));
     }
     return metadata;
   }
